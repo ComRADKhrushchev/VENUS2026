66f89cd667979d6b0dbb62547d1e59a8735b9a21
zzchen <13614@users.noreply.github.com>
2026-08-26 17:32:08 +0800

    src_VENUS/ROTATEZ.f: 绕质心欧拉角随机旋转子步骤

---
+++ src_VENUS/ROTATEZ.f
@@ -0,0 +1,58 @@
+      SUBROUTINE ROTATEZ(N)
+      use venus_params
+      use venus_data
+      IMPLICIT DOUBLE PRECISION (A-H,O-Z)
+C
+C         RANDOMLY ROTATE A MOLECULE ABOUT ITS CENTER OF MASS BY
+C         EULER'S ANGLES.
+C
+C
+C         CENTER OF MASS COORDINATES(QQ) AND MOMENTA(PP) ARE PASSED FROM
+C         SUBROUTINES CENMAS AND ANGVEL THROUGH COMMON BLOCK WASTE.
+C
+      RAND=RAND0(ISEED)
+      PHI=TWOPI*RAND
+      SNPHI=SIN(PHI)
+      CSPHI=COS(PHI)
+      DO I=1,N
+      J=3*LL(I)
+         Q(J-2)=QQ(J-2)*CSPHI-QQ(J-1)*SNPHI
+         Q(J-1)=QQ(J-2)*SNPHI+QQ(J-1)*CSPHI
+         QQ(J-2)=Q(J-2)
+         QQ(J-1)=Q(J-1)
+         P(J-2)=PP(J-2)*CSPHI-PP(J-1)*SNPHI
+         P(J-1)=PP(J-2)*SNPHI+PP(J-1)*CSPHI
+         PP(J-2)=P(J-2)
+         PP(J-1)=P(J-1)
+      ENDDO
+      RETURN
+      END
+	  
+	  SUBROUTINE ROTATEZ_b(PHI,N)
+      use venus_data, only: PHI_H => PHI
+      use venus_data
+      IMPLICIT DOUBLE PRECISION (A-H,O-Z)
+C
+C         RANDOMLY ROTATE A MOLECULE ABOUT ITS CENTER OF MASS BY
+C         EULER'S ANGLES.
+C
+C
+C         CENTER OF MASS COORDINATES(QQ) AND MOMENTA(PP) ARE PASSED FROM
+C         SUBROUTINES CENMAS AND ANGVEL THROUGH COMMON BLOCK WASTE.
+C
+
+      SNPHI=SIN(PHI)
+      CSPHI=COS(PHI)
+      DO I=1,N
+      J=3*LL(I)
+         Q(J-2)=QQ(J-2)*CSPHI-QQ(J-1)*SNPHI
+         Q(J-1)=QQ(J-2)*SNPHI+QQ(J-1)*CSPHI
+         QQ(J-2)=Q(J-2)
+         QQ(J-1)=Q(J-1)
+         P(J-2)=PP(J-2)*CSPHI-PP(J-1)*SNPHI
+         P(J-1)=PP(J-2)*SNPHI+PP(J-1)*CSPHI
+         PP(J-2)=P(J-2)
+         PP(J-1)=P(J-1)
+      ENDDO
+      RETURN
+      END
